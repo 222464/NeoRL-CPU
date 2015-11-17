5e5dbb3aef90ab2600e5ee600810820d86bd43d8
Crystalline Emerald <crystalline.emerald@gmail.com>
2015-11-18 00:20:16 +0300

    basic commandline settings, prediction works, sampling does not work yet

--- NeoRL-CPU/source/examples/TextPrediction.cpp
+++ NeoRL-CPU/source/examples/TextPrediction.cpp
@@ -4,6 +4,8 @@
 
 #include <neo/PredictiveHierarchy.h>
 
+#include "../libs/argparse.hpp"
+
 #include <time.h>
 #include <iostream>
 #include <random>
@@ -14,15 +16,98 @@
 
 #include <algorithm>
 
-int main() {
-	// RNG
-	std::mt19937 generator(time(nullptr));
+void train(neo::PredictiveHierarchy ph, std::mt19937 generator,
+           std::string& test, int epochs, int numInputs, int inputsRoot,
+           int minimum, int maximum) {
+	
+    for (size_t k = 0; k < epochs; k++) {
+        for (size_t i = 0; i < test.length(); i++) {
+            for (int j = 0; j < inputsRoot * inputsRoot; j++) {
+                ph.setInput(j, 0.0f);
+            }
+            
+            int index = test[i] - minimum;
 
+            ph.setInput(index, 1.0f);
+
+            ph.simStep(generator);
+
+            int predIndex = 0;
+            for (int i = 0; i < numInputs; i++) {
+                if (ph.getPrediction(i) > ph.getPrediction(predIndex))
+                    predIndex = i;
+            }
+			
+            char predChar = predIndex + minimum;
+
+            std::cout << predChar;
+        }
+        std::cout << "\n";
+    }
+}
+
+void sample(neo::PredictiveHierarchy ph, std::mt19937 generator,
+           char seed, int nSamples, int numInputs, int inputsRoot,
+           int minimum, int maximum) {
+    
 	std::uniform_real_distribution<float> dist01(0.0f, 1.0f);
 
-	// ---------------------------------- Load the Corpus ----------------------------------
+    for (int j = 0; j < inputsRoot * inputsRoot; j++) {
+        ph.setInput(j, 0.0f);
+    }
+    ph.setInput(seed - minimum, 1.0f);//dist01(generator)*0.1+0.9);
+    ph.simStep(generator);
+    
+    std::cout << "seed: " << seed << " sample: ";
+    
+    for (size_t i = 1; i < nSamples; i++) {
+        
+        ph.simStep(generator);
 
-	std::ifstream fromFile("corpus.txt");
+        int predIndex = 0;
+        for (int i = 0; i < numInputs; i++) {
+            if (ph.getPrediction(i) > ph.getPrediction(predIndex))
+                predIndex = i;
+        }
+        
+        char predChar = predIndex + minimum;
+        std::cout << predIndex << " " << predChar << " ";
+		
+        for (int j = 0; j < inputsRoot * inputsRoot; j++) {
+            ph.setInput(j, ph.getPrediction(j));
+        }
+    }
+    
+    std::cout << std::endl;
+}
+
+int main(int argc, const char** argv) {
+	
+    // Load the command line config
+    
+    ArgumentParser parser;
+        
+    parser.addArgument("-e", "--epochs", 1);
+    parser.addArgument("-s", "--seed", 1);
+    parser.addArgument("-l", "--layers", 1);
+    parser.addArgument("-S", "--samples", 1);
+    parser.addArgument("-c", "--corpus", 1);
+    parser.addArgument("--nlayers", 1);
+    parser.addArgument("--ifbradius", 1);
+    parser.addArgument("--lw", 1);
+    parser.addArgument("--lh", 1);
+    
+    parser.parse(argc, argv);
+    
+	// RNG
+    unsigned int seed = std::atoi(parser.retrieve("seed", std::to_string(time(nullptr))).c_str());
+	std::mt19937 generator(seed);
+
+	std::uniform_real_distribution<float> dist01(0.0f, 1.0f);
+    
+	// ---------------------------------- Load the Corpus ----------------------------------
+	std::string corpusPath = parser.retrieve("corpus", "corpus.txt");
+	std::ifstream fromFile(corpusPath);
 
 	fromFile.seekg(0, std::ios::end);
 	size_t size = fromFile.tellg();
@@ -44,61 +129,39 @@ int main() {
 
 	// Organize inputs into a square input region
 	int numInputs = maximum - minimum + 1;
-
 	int inputsRoot = std::ceil(std::sqrt(static_cast<float>(numInputs)));
-
-	std::vector<neo::PredictiveHierarchy::LayerDesc> layerDescs(3);
-
-	// Fill out layer descriptions
-	layerDescs[0]._width = 16;
-	layerDescs[0]._height = 16;
 	
-	layerDescs[1]._width = 16;
-	layerDescs[1]._height = 16;
-
-	layerDescs[2]._width = 16;
-	layerDescs[2]._height = 16;
-
+	// Fill out layer descriptions
+	int nLayers = std::atoi(parser.retrieve("nlayers", "3").c_str());
+	int layerW = std::atoi(parser.retrieve("lw", "16").c_str());
+	int layerH = std::atoi(parser.retrieve("lh", "16").c_str());
+	int inFeedBackRadius = std::atoi(parser.retrieve("ifbradius", "16").c_str());
+	
+	std::vector<neo::PredictiveHierarchy::LayerDesc> layerDescs(nLayers);
+	
+	for (int i = 0; i < nLayers; i++) {
+		layerDescs[i]._width = layerW;
+		layerDescs[i]._height = layerH;
+	}
+    
 	neo::PredictiveHierarchy ph;
 
-	ph.createRandom(inputsRoot, inputsRoot, 16, layerDescs, -0.01f, 0.01f, 0.01f, 0.05f, 0.1f, generator);
+	ph.createRandom(inputsRoot, inputsRoot, inFeedBackRadius, layerDescs, -0.01f, 0.01f, 0.01f, 0.05f, 0.1f, generator);
 
 	// ---------------------------------- Iterate Over Corpus ----------------------------------
-
-	// Current character index
-	int current = 0;
-
-	for (size_t i = 0; i < 1000000; i++) {
-		for (int i = 0; i < inputsRoot * inputsRoot; i++)
-			ph.setInput(i, 0.0f);
-
-		int index = test[current] - minimum;
-
-		ph.setInput(index, 1.0f);
-
-		ph.simStep(generator);
-
-		int predIndex = 0;
-
-		for (int i = 1; i < numInputs; i++)
-			if (ph.getPrediction(i) > ph.getPrediction(predIndex))
-				predIndex = i;
-
-		char predChar = predIndex + minimum;
-
-		std::cout << predChar;
-
-		current = (current + 1) % test.length();
-
-		float error = 1.0f;
-
-		if (predChar == test[current])
-			error = 0.0f;
-
-		if (current == 0)
-			std::cout << "\n";
-	}
-
+    int numEpochs = std::atoi(parser.retrieve("epochs", "10").c_str());
+    int numSamples = std::atoi(parser.retrieve("samples", "10").c_str());
+    
+    std::cout << "NeoRL text prediction experiment" << std::endl;
+    std::cout << "Corpus: " << corpusPath << " size: " << test.length() << " minChar: " << minimum << " maxChar: " << maximum << std::endl;
+    std::cout << "Model: nLayers: " << nLayers << " layerW: " << layerW << " layerH: " << layerH << " inFeedbackRadius: " << inFeedBackRadius << std::endl;
+    
+    train(ph, generator, test, numEpochs, numInputs, inputsRoot, minimum, maximum);
+    
+    for (int i = 0; i < numSamples; i++) {
+        sample(ph, generator, 'I', test.length(), numInputs, inputsRoot, minimum, maximum);
+    }
+    
 	return 0;
 }
 
