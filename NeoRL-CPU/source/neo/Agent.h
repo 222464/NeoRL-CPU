f4a4f5a6c43666daaed5a14af4afcbca897b9469
Eric Laukien <eric@rocketcitystudios.com>
2015-11-18 16:00:16 -0500

    Added RL stuff

--- NeoRL-CPU/source/neo/Agent.h
+++ NeoRL-CPU/source/neo/Agent.h
@@ -1,10 +1,15 @@
 #pragma once
 
 #include "SparseCoder.h"
+#include "Column.h"
 
 namespace neo {
-	class PredictiveHierarchy {
+	class Agent {
 	public:
+		enum ColumnAction {
+			_attention = 0, _learnPrediction, _signal, _numColumnActions
+		};
+
 		struct Connection {
 			unsigned short _index;
 
@@ -13,6 +18,16 @@ namespace neo {
 
 		struct LayerDesc {
 			int _width, _height;
+
+			int _cellsPerColumn;
+			float _columnSparsity;
+			int _columnIter;
+			float _columnLeak;
+			float _columnGamma;
+			float _columnGammaLambda;
+			float _columnFeedForwardAlpha, _columnLateralAlpha, _columnThresholdAlpha;
+			float _columnQAlpha, _columnActionAlpha;
+			float _columnExplorationStdDev, _columnExplorationBreakChance;
 
 			int _receptiveRadius, _recurrentRadius, _lateralRadius, _predictiveRadius, _feedBackRadius;
 
@@ -33,12 +48,17 @@ namespace neo {
 
 			LayerDesc()
 				: _width(16), _height(16),
+				_cellsPerColumn(16), _columnSparsity(0.125f), _columnIter(7),
+				_columnLeak(0.1f),
+				_columnFeedForwardAlpha(0.04f), _columnLateralAlpha(0.1f), _columnThresholdAlpha(0.01f),
+				_columnQAlpha(0.01f), _columnActionAlpha(0.1f),
+				_columnExplorationStdDev(0.05f), _columnExplorationBreakChance(0.01f),
 				_receptiveRadius(4), _recurrentRadius(4), _lateralRadius(4), _predictiveRadius(4), _feedBackRadius(4),
 				_learnFeedForward(0.01f), _learnRecurrent(0.01f), _learnLateral(0.05f),
 				_learnFeedBack(0.1f), _learnPrediction(0.03f),
 				_sdrIter(30),
 				_sdrLeak(0.1f), _sdrLambda(0.95f), _sdrHiddenDecay(0.01f), _sdrWeightDecay(0.0f), _sdrMaxWeightDelta(0.5f),
-				_sdrSparsity(0.02f), _sdrLearnThreshold(0.01f),
+				_sdrSparsity(0.05f), _sdrLearnThreshold(0.01f),
 				_sdrBaselineDecay(0.01f),
 				_sdrSensitivity(6.0f)
 			{}
@@ -49,6 +69,8 @@ namespace neo {
 			std::vector<Connection> _predictiveConnections;
 
 			Connection _bias;
+
+			Column _column;
 
 			float _state;
 			float _statePrev;
@@ -67,6 +89,8 @@ namespace neo {
 			std::vector<Connection> _feedBackConnections;
 
 			Connection _bias;
+
+			Column _column;
 
 			float _state;
 			float _statePrev;
@@ -97,17 +121,31 @@ namespace neo {
 
 
 	public:
+		// First layer columns
+		int _cellsPerColumn;
+		float _columnSparsity;
+		int _columnIter;
+		float _columnLeak;
+		float _columnGamma;
+		float _columnGammaLambda;
+		float _columnFeedForwardAlpha, _columnLateralAlpha, _columnThresholdAlpha;
+		float _columnQAlpha, _columnActionAlpha;
+		float _columnExplorationStdDev, _columnExplorationBreakChance;
+
 		float _learnInputFeedBack;
 
-		PredictiveHierarchy()
-			: _learnInputFeedBack(0.1f)
+		Agent()
+			: _cellsPerColumn(16), _columnSparsity(0.125f), _columnIter(7),
+			_columnLeak(0.1f),
+			_columnFeedForwardAlpha(0.04f), _columnLateralAlpha(0.1f), _columnThresholdAlpha(0.01f),
+			_columnQAlpha(0.01f), _columnActionAlpha(0.1f),
+			_columnExplorationStdDev(0.05f), _columnExplorationBreakChance(0.01f),
+			_learnInputFeedBack(0.1f)
 		{}
 
 		void createRandom(int inputWidth, int inputHeight, int inputFeedBackRadius, const std::vector<LayerDesc> &layerDescs, float initMinWeight, float initMaxWeight, float initMinInhibition, float initMaxInhibition, float initThreshold, std::mt19937 &generator);
 
-		void simStep(std::mt19937 &generator, bool learn = true);
-
-		void simStepGenerate(std::mt19937 &generator, float noise);
+		void simStep(float reward, std::mt19937 &generator, bool learn = true);
 
 		void setInput(int index, float value) {
 			_layers.front()._sdr.setVisibleState(index, value);
